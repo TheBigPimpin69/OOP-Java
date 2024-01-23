4e30f12a3d89cca93d68a93283a0c0ca9dde2127
Eamonn Patterson <153055060+TheBigPimpin69@users.noreply.github.com>
2024-01-23 17:53:00 -0500

    Add files via upload

---
+++ lab_1(OOP).java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+class lab_1 {
+    public static void main(String args[]){
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("What is your name?");
+        String name = scanner.nextLine(); 
+        //System.out.println("Hello " + name + ", welcome to the COSC1200 Class");
+
+        System.out.println("What's your age?");
+        String ageString = scanner.nextLine(); 
+
+      
+        int age = Integer.parseInt(ageString);
+
+        if (age < 25){
+            System.out.println("Hello " + name + ", welcome to the COSC1200 Class, you are very young.");
+        }
+        else if (age >= 25 && age < 45){
+            System.out.println("Hello " + name + ", welcome to the COSC1200 Class, you still have hope.");
+        }
+        else if (age >= 45 && age <60){
+            System.out.println("Hello " + name + ", welcome to the COSC1200 Class, VERY OLD");
+        }
+        else if (age > 60){ 
+        System.out.println("Hello " + name + ", welcome to the COSC1200 Class, deaths door");
+        }
+        scanner.close(); 
+    }
+}
+
